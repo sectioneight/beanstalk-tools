5e4a0725c7b6c93b30e25a9eec4935468ad88dbc
Aiden Scandella <aiden@causes.com>
2013-02-07 09:29:55 -0800

    add peek.rb
    Change-Id: I968dfd9d77fab3c5321807d6cb9c9e2198932001

---
+++ bin/peek.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'beanstalk-client'
+
+BS=Beanstalk::Pool.new $*
+
+def abbrev(code, to=70)
+  if code && code.length > to
+    code.slice(0, to-3) + "..."
+  else
+    code
+  end
+end
+
+loop do
+  job = BS.reserve
+  code = case job.ybody
+  when Hash
+    job.ybody[:code]
+  else
+    job.ybody.inspect
+  end
+  puts code
+  job.release(job.pri, 60)
+end
